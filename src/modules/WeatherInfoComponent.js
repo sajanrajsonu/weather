0b09ddd430f08ccb1247bf489a09c3847c613a0a
Sajan Raj Sonu <sajanrajsonu@gmail.com>
2022-06-30 07:24:25 +0530

    Update WeatherInfoComponent.js

--- src/modules/WeatherInfoComponent.js
+++ src/modules/WeatherInfoComponent.js
@@ -3,11 +3,11 @@ import styled from "styled-components";
 import {WeatherIcons} from "../App";
 
 export const WeatherInfoIcons = {
-    sunset: "/icons/temp.svg",
-    sunrise: "/icons/temp.svg",
-    humidity: "/icons/humidity.svg",
-    wind: "/icons/wind.svg",
-    pressure: "/icons/pressure.svg",
+    sunset: "/weather/icons/temp.svg",
+    sunrise: "/weather/icons/temp.svg",
+    humidity: "/weather/icons/humidity.svg",
+    wind: "/weather/icons/wind.svg",
+    pressure: "/weather/icons/pressure.svg",
 };
 
 
